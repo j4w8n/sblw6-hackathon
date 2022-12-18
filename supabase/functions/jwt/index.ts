14f9cd44fb50084e045e800a35920e3345fc46bb
Jason Creviston <jason@malynium.com>
2022-12-18 13:05:11 -0500

    tweaks and tidy up

--- supabase/functions/jwt/index.ts
+++ supabase/functions/jwt/index.ts
@@ -2,6 +2,7 @@ import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
 import { create } from 'https://deno.land/x/djwt@v2.8/mod.ts'
 import { crypto } from 'https://deno.land/std@0.167.0/crypto/mod.ts'
 import { supabaseAdminClient } from '../shared/supabase.ts'
+import { validateJson } from '../shared/utils.ts'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
@@ -19,39 +20,53 @@ const key = await crypto.subtle.generateKey(
 const jwk = await crypto.subtle.exportKey('jwk', key)
 
 serve(async (req: Request): Promise<Response> => {
-  if (req.method === 'OPTIONS') {
+  const { method } = req
+  if (method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders })
   }
 
-  const json = await req.json()
-  const jwt = await create(
-    { alg: 'HS512', typ: 'JWT' }, 
-    {
-      iss: 'rockthecode.dev',
-      aud: 'sblw6',
-      iat: Math.floor(Date.now() / 1000),
-      nbf: (Date.parse('2022-12-12T00:00:00') / 1000),
-      exp: (Date.parse('2022-12-26T00:00:00') / 1000),
-      ...json
-    }, 
-    key
-  )
+  if (method === 'POST') {
+    const json = await validateJson(req)
+    if (json.error) {
+      return new Response(
+        JSON.stringify({ jwt: null, key: null, error: json.error}),
+        { headers }
+      )
+    }
 
-  /* add info to database for later verification */
-  const { error } = await supabaseAdminClient.from('jwts').insert([
-    { jwt, attendee: json.attendee }
-  ])
+    const jwt = await create(
+      { alg: 'HS512', typ: 'JWT' }, 
+      {
+        iss: 'rockthecode.dev',
+        aud: 'sblw6',
+        iat: Math.floor(Date.now() / 1000),
+        nbf: (Date.parse('2022-12-12T00:00:00') / 1000),
+        exp: (Date.parse('2022-12-26T00:00:00') / 1000),
+        ...json
+      }, 
+      key
+    )
 
-  if (error) {
-    console.log(error)
+    /* add info to database for later verification */
+    const { error } = await supabaseAdminClient.from('jwts').insert([
+      { jwt, attendee: json.attendee }
+    ])
+    if (error) {
+      console.log(error)
+      return new Response(
+        JSON.stringify({ jwt: null, key: null, error }),
+        { headers },
+      )
+    }
+
     return new Response(
-      JSON.stringify({ jwt: null, key: null, error }),
+      JSON.stringify({ jwt, key: jwk.k, error: null }),
       { headers },
     )
   }
 
   return new Response(
-    JSON.stringify({ jwt, key: jwk.k, error: null }),
-    { headers },
+    JSON.stringify({ message: `HTTP method ${method} not supported in this API.`}),
+    { headers }
   )
 })
---
+++ supabase/functions/shared/utils.ts
@@ -0,0 +1,22 @@
+type jsonResponse = { 
+  attendee: string | null
+  start_date: string | null
+  error: Error | null
+}
+
+export const validateJson = async (request: Request): Promise<jsonResponse> => {
+  const json = request.body ? await request.json().catch((err: Error) => {
+    if (err.name === 'SyntaxError') {
+      console.error(err)
+      return { attendee: null, start_date: null, error: 'Invalid JSON' }
+    } else {
+      console.error(
+        { data: { url: request.url }, error: err.message }
+      )
+      return { attendee: null, start_date: null, error: err.message }
+    }
+  }): { attendee: null, start_date: null, error: 'Body is null'}
+
+  json.error = null
+  return json
+}
